01ae9d32a12ba06623b5952c31e3319690ec7451
Troncador <benjamin.almarza@alumnos.usm.cl>
2025-03-12 09:28:03 +1100

    simplify 'init'

--- src/galaxy/actions/InitAction.ts
+++ src/galaxy/actions/InitAction.ts
@@ -1,57 +1,57 @@
 import { InitialBoard, InitPayload, SpaceState } from "../Types";
-import { Hex, HexUtils } from "../../hex";
+import { HexUtils } from "../../hex";
 import raceData from "../../assets/data/raceData.json"
 
 const MECATOL_REX_TILE = "18"
 const EMPTY_TILE = "0"
 
 function init({ map: { home_worlds, hyperlane_tiles, primary_tiles, secondary_tiles, tertiary_tiles, quaternary_tiles }, factions }: InitPayload ): InitialBoard {
-
-    let spaceMap = new Map<string, SpaceState>()
-
-    spaceMap.set(new Hex(0, 0).toString(), {
+    const mecatol = {
         position: 0,
         source: MECATOL_REX_TILE,
-        coordinates: new Hex(0, 0),
         rotate: 0
-    })
-
-    function addEmptyTile (position: number) {
-        let hex = HexUtils.getHexFromPosition(position)
-        spaceMap.set(hex.toString(), {
-            position: position,
-            source: EMPTY_TILE,
-            coordinates: hex,
-            rotate: 0
-        })
     }
-    primary_tiles.forEach(addEmptyTile)
-    secondary_tiles.forEach(addEmptyTile)
-    tertiary_tiles.forEach(addEmptyTile)
-    quaternary_tiles.forEach(addEmptyTile)
     
-    hyperlane_tiles.forEach((tile: [number, string, number]) => {
-        let hex = HexUtils.getHexFromPosition(tile[0])
-        spaceMap.set(hex.toString(), {
-            position: tile[0],
-            source: tile[1],
-            coordinates: hex,
-            rotate: tile[2] * 60
+
+    const rings = [...primary_tiles, ...secondary_tiles, ...tertiary_tiles, ...quaternary_tiles]
+        .map((position) => {
+            return {
+                position: position,
+                source: EMPTY_TILE,
+                rotate: 0
+            }
         })
+
+    const hyperLane = hyperlane_tiles.map(([position, source, rotation]: [number, string, number]) => {
+        return { position, source, rotate: rotation * 60 }
     })
 
-    home_worlds.forEach((position: number, i: number) => {
+    const homeWorlds = home_worlds.map((position: number, i: number) => {
         const faction = factions[i]
         const system = raceData.raceToHomeSystemMap[faction]
 
-        let hex = HexUtils.getHexFromPosition(position)
-        spaceMap.set(hex.toString(), {
+        return {
             position: position,
             source: `${system}`,
-            coordinates: hex,
             rotate: 0
-        })
+        }
     })
+
+    const allTiles = [mecatol, ...rings, ...hyperLane, ...homeWorlds].map((value) => {
+        const { position } = value;
+        const hex = HexUtils.getHexFromPosition(position)
+        return [
+            hex.toString(),
+            {
+                ...value,
+                coordinates: hex
+            }
+        ] as [string, SpaceState]
+    })
+
+    const spaceMap = new Map<string, SpaceState>(allTiles)
+
+
 
     const homeTiles = home_worlds.map(position => HexUtils.getHexFromPosition(position))
     const primaryTiles = primary_tiles.map(position => HexUtils.getHexFromPosition(position))
